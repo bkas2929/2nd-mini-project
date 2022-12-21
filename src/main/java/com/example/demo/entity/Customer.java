03e86ac0221fc6a747825c1938fe394c82fd5195
bikash pradhan <bikash.k.pradhan@gmail.com>
2022-12-21 08:00:35 +0530

    first commit

---
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+public class Customer {
+	
+	private Integer customerId;
+	private String customerName;
+	private String customerEmail;
+	private long customerContactNumber;
+	private String gender;
+	private String customerSSN;
+	
+
+}
